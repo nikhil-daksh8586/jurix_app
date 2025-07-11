60859bbde9991067a5e38da8150918867e9280c5
Nikhil <nikhildaksh8586@gmail.com>
2025-07-11 22:53:25 +0530

    main commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,16 @@
 import 'package:flutter/material.dart';
-import 'package:firebase_core/firebase_core.dart';
-import 'package:get/get.dart';
+
 
 import 'app.dart';
-import 'firebase_options.dart';
 
-Future<void> main() async {
+void main() {
 
   // Todo: Add Widgets Binding
   // Todo: Init Local Storage
   // Todo: Init Payment Methods
   // Todo: Await Native Splash
   // Todo: Initialize Firebase
-  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform).then(
-      (FirebaseApp value) => Get.put(AuthanticationRepository()),
-  );
+  
   // Todo: Initialize Authentication
 
   runApp(const App());
